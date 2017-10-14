b89fbfadfdd5e6dac6238312cec67c9144ff6cd5
AH1N1 <wolf11-1995@o2.pl>
2017-10-14 18:16:07 +0200

    uploading core

---
+++ src/main/java/model/BarcodeInfo.java
@@ -0,0 +1,25 @@
+package model;
+
+public class BarcodeInfo {
+    private String barcode;
+    private int amount;
+
+    public BarcodeInfo() {
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
